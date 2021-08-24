93374019e1c5afa0f20731ef7de7d8dd4f7064c3
badal Jha <badalkjha@gmail.com>
2021-08-24 16:18:31 +0530

    Body sections added

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -1,6 +1,29 @@
 import React from "react";
-
+import About from "./about/about";
+import "./body.css";
+import Contact from "./contact/contact";
+import Projects from "./projects/project";
+import Skills from "./skills/skill";
+import Work from "./work/work";
 const Body = () => {
-  return <div>Body</div>;
+  return (
+    <div className="body">
+      <section id="about">
+        <About />
+      </section>
+      <section id="projects">
+        <Projects />
+      </section>
+      <section id="skills">
+        <Skills />
+      </section>
+      <section id="work">
+        <Work />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+    </div>
+  );
 };
 export default Body;
