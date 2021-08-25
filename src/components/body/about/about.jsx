bfad4ddeb05bcf600a8be44af401268592444b84
badal Jha <badalkjha@gmail.com>
2021-08-25 14:50:56 +0530

    about section added

--- src/components/body/about/about.jsx
+++ src/components/body/about/about.jsx
@@ -1,6 +1,34 @@
 import React from "react";
-
+import photo from "../../../assets/photo.png";
+import "./about.css";
+import Typical from "react-typical";
 const About = () => {
-  return <div>This is about</div>;
+  return (
+    <div className="about">
+      <div className="about-top">
+        <div className="about-info">
+          <p>
+            I Build
+            <Typical
+              className="moving-text"
+              loop={Infinity}
+              wrapper="b"
+              steps={[
+                " React Applications.",
+                1000,
+                " Blockchain Applicatons.",
+                1000,
+              ]}
+            />
+          </p>
+          Hello, This is Badal Jha.
+          <br />I am Learning reactjs and blockchain appplication development.
+        </div>
+        <div className="about-photo">
+          <img src={photo} alt="Badal" className="photo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default About;
