42b30a041d1cab93b1c36bb63f57548842f8ff82
badal Jha <badalkjha@gmail.com>
2021-09-13 17:55:58 +0530

    skills components completed

--- src/components/body/skills/skill.jsx
+++ src/components/body/skills/skill.jsx
@@ -1,6 +1,38 @@
 import React from "react";
+import SectionSeperator from "../../comman/sectionSeperator/sectionSeperator";
+import skillsData from "../../data/skills";
+import "./skills.css";
+
 const Skills = () => {
-  return <div>This is Skills</div>;
+  return (
+    <div className="skills">
+      <div className="section-title">Skills</div>
+      <SectionSeperator />
+      <div className="skills-container">
+        {skillsData.map((item) => {
+          return (
+            <div className="skills-section">
+              <div className="skills-type">{item.type}</div>
+              <div className="skills-icons">
+                {item.skills.map((skill) => {
+                  return (
+                    <div className="skill-box">
+                      <img
+                        src={skill.icon}
+                        alt={skill.name}
+                        className="skill-icon"
+                      />
+                      <label>{skill.name}</label>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
---
+++ src/components/data/skills.jsx
@@ -0,0 +1,54 @@
+const skillsData = [
+  {
+    type: "FrontEnd",
+    skills: [
+      {
+        name: "JavaScript",
+        icon: require("../../assets/icons/javascript.png").default,
+      },
+      {
+        name: "ReactJs",
+        icon: require("../../assets/icons/ReactJS.png").default,
+      },
+      {
+        name: "HTML",
+        icon: require("../../assets/icons/html.png").default,
+      },
+      {
+        name: "CSS",
+        icon: require("../../assets/icons/css.png").default,
+      },
+      {
+        name: "C++",
+        icon: require("../../assets/icons/c++.png").default,
+      },
+    ],
+  },
+  {
+    type: "Backend",
+    skills: [
+      {
+        name: "NodeJs",
+        icon: require("../../assets/icons/NodeJS.png").default,
+      },
+      {
+        name: "ExpressJs",
+        icon: require("../../assets/icons/express.png").default,
+      },
+    ],
+  },
+  {
+    type: "Blockchain development",
+    skills: [
+      {
+        name: "Etherium",
+        icon: require("../../assets/icons/etherium.png").default,
+      },
+      {
+        name: "Solidity",
+        icon: require("../../assets/icons/solidity.png").default,
+      },
+    ],
+  },
+];
+export default skillsData;
